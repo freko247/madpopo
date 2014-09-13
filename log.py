37a32ec3d69e8e8246dcac0f89a1f176b194dd1c
freko <freko247@gmail.com>
2014-09-13 07:09:17 +0200

    Added useful console printing, by creating 2nd log handler.

--- log.py
+++ log.py
@@ -2,9 +2,21 @@
 import logging
 import os
 
+# Create logger
 logger = logging.getLogger('madpopo')
+logger.setLevel(logging.DEBUG)
+
+# Create log handlers and set log level
 hdlr = logging.FileHandler(os.getcwd() + '/madpopo.log')
+hdlr.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+
+# Add formatting
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+# Add handlers
 logger.addHandler(hdlr)
-logger.setLevel(logging.DEBUG)
+logger.addHandler(ch)
