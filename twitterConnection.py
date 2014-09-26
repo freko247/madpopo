9d99adf51d054d9ba190a79d6d13b9803c2720e0
freko <freko247@gmail.com>
2014-09-26 11:01:43 +0200

    Added class in connection script for creating stream connection.

--- twitterConnection.py
+++ twitterConnection.py
@@ -6,3 +6,9 @@ auth = twitter.oauth.OAuth(config.OAUTH_TOKEN, config.OAUTH_TOKEN_SECRET,
                            config.CONSUMER_KEY, config.CONSUMER_SECRET)
 
 twitter_api = twitter.Twitter(auth=auth)
+
+
+class TwitterStream():
+    """Class for creating differrent stream connections"""
+    def __init__(self, domain, auth=auth, ):
+        self.stream = twitter.TwitterStream(domain=domain, auth=auth)
