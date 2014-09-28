8b535003cbaf037c26c21c1d139f96d88f484438
freko <freko247@gmail.com>
2014-09-28 14:39:53 +0200

    Added db model for tweets.

---
+++ models.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime, Float
+
+Base = declarative_base()
+
+
+class Tweet(Base):
+    '''Table, that stores following information about tweet:
+    ["text"],
+    ["coordinates"] -> [lon][lat],
+    ["retweet_count"],
+    ["id"],
+    ["created_at"],
+    ["user"]["id"].'''
+    __tablename__ = "tweet"
+    id = Column(Integer, primary_key=True)
+    text = Column(String(length=500))
+    lon = Column(Float())
+    lat = Column(Float())
+    retweet_count = Column(Integer())
+    tweet_id = Column(Integer())
+    created_at = Column(DateTime)
+    user_id = Column(Integer())
