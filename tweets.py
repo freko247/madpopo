6a9897dc11271eb4cf83caf24b6f9bdf041e3c55
freko <freko247@gmail.com>
2014-09-29 13:57:20 +0200

    Catch exceptions when trying to store timeline and log errors.

--- tweets.py
+++ tweets.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 from dateutil import parser
+import sys
 
 import db
 from log import logger
@@ -8,19 +9,23 @@ from twitterConnection import TwitterStream
 
 
 def storeTimeline():
-    logger.info('Starting to store timeline data')
-    twitter_stream = TwitterStream(domain='userstream.twitter.com')
-    db.init_db()
-    for msg in twitter_stream.stream.user(following=True):
-        if not msg.get('friends'):
-            tweet = Tweet()
-            tweet.text = msg['text'].encode('utf-8')
-            tweet.created_at = parser.parse(msg['created_at']).replace(tzinfo=None)
-            if msg.get('coordinates'):
-                tweet.lat = msg['coordinates']['coordinates'][0]
-                tweet.lon = msg['coordinates']['coordinates'][1]
-            tweet.tweet_id = msg['id']
-            tweet.retweet_count = msg['retweet_count']
-            tweet.user_id = msg['user']['id']
-            db.session.add(tweet)
-            db.session.commit()
+    try:
+        logger.info('Starting to store timeline data')
+        twitter_stream = TwitterStream(domain='userstream.twitter.com')
+        db.init_db()
+        for msg in twitter_stream.stream.user(following=True):
+            if not msg.get('friends'):
+                tweet = Tweet()
+                tweet.text = msg['text'].encode('utf-8')
+                tweet.created_at = parser.parse(msg['created_at']).replace(tzinfo=None)
+                if msg.get('coordinates'):
+                    tweet.lat = msg['coordinates']['coordinates'][0]
+                    tweet.lon = msg['coordinates']['coordinates'][1]
+                tweet.tweet_id = msg['id']
+                tweet.retweet_count = msg['retweet_count']
+                tweet.user_id = msg['user']['id']
+                db.session.add(tweet)
+                db.session.commit()
+    except:
+        logger.info("Unexpected error: %s" % sys.exc_info()[0])
+        raise
