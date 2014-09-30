0c0e7ca728359fe8732c29a79bbbf865bb8c8b06
freko <freko247@gmail.com>
2014-09-30 18:38:51 +0200

    Added retweet function and function for getting popular tweets of interesting subjects.

--- tweets.py
+++ tweets.py
@@ -2,11 +2,13 @@
 from dateutil import parser
 import sys
 import os
+import datetime
 
 import db
 from log import logger
 from models import Tweet
 from twitterConnection import TwitterStream
+from twitterConnection import twitter_api
 
 
 def storeTimeline():
@@ -31,6 +33,24 @@ def storeTimeline():
     except:
         logger.error("Unexpected error: %s" % sys.exc_info()[0])
         raise
+
+
+def getTweets(filter, until=None):
+    searches = []
+    today = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
+    for query in filter:
+        searches.append(twitter_api.search.tweets(q=query,
+                                                  lang='en',
+                                                  result_type='popular',
+                                                  count=15,
+                                                  until=until or today
+                                                  )
+                        )
+    return searches
+
+
+def reTweet(id):
+    twitter_api.statuses.retweet(id=id)
 
 
 def main():
