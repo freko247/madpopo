1bac77e923cd86c068c740a7ee27ac0b99371137
Ubuntu <ubuntu@ip-172-31-20-61.eu-west-1.compute.internal>
2014-10-01 07:56:57 +0000

    Fix log bug

--- retweeter.py
+++ retweeter.py
@@ -24,7 +24,7 @@ def main():
         # Retweet the most positive tweet
         tweet = analyzed[0][1]
         tweets.reTweet(tweet)
-        logger.info('%s, retweeting tweet_id: %s, with sentiment %s ' % (i+1, tweet[1], tweet[0]))
+        logger.info('Retweeting tweet_id: %s, with sentiment %s ' % (tweet[1], tweet[0]))
     except Exception, err:
         logger.error('%s: %s' % (Exception, err))
 
