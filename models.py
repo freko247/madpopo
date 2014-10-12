fc79cf281fe40c6f6919d331acaa116751a959b0
freko <freko247@gmail.com>
2014-10-12 08:56:05 +0200

    Added user, and language table.

--- models.py
+++ models.py
@@ -23,3 +23,32 @@ class Tweet(Base):
     tweet_id = Column(String(20))
     created_at = Column(DateTime)
     user_id = Column(String(20))
+
+
+class Users(Base):
+    ''' Stores user information
+    activity = Tweet frequency
+    followed = Date when user was followed
+    followed_back = Date when user followed back
+    followers = Number of followers_count
+    friends = Number of friends_count
+    join_date = User join date
+    language = User language
+    tweets = Tweet count
+    user_id = User id
+    '''
+    activity = Column(Float())          # tweet_frequency
+    followed = Column(DateTime)         # Date when user was followed
+    followed_back = Column(DateTime)    # Date when user followed back
+    followers = Column(Integer())       # number of followers_count
+    friends = Column(Integer())         # number of friends_count
+    join_date = Column(DateTime)        # User join date
+    language = Column(Integer())        # lang
+    tweets = Column(Integer())          # statuses_count
+    user_id = Column(Integer, primary_key=True)
+
+
+class Language(Base):
+    '''Language lookup table'''
+    id = Column(Integer(), primary_key=True)
+    language = Column(String(5))
