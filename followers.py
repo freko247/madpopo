1ce06542d429db1b2964e17d2cfe0d9d80fa1085
freko <freko247@gmail.com>
2014-10-14 09:37:50 +0200

    Use user_id instead of screen name when following, (for consistency).

--- followers.py
+++ followers.py
@@ -4,23 +4,25 @@ from operator import itemgetter
 
 from log import logger
 from twitterConnection import twitter_api
+import db
+from models import User
 '''This module provides basic functionality for management of followers.'''
 
 
-def getFollowers(screen_name, destinationFile=None):
+def getFollowers(user_id, destinationFile=None):
     '''Function gets a list of users following a given user.'''
     found_followers = []
-    logger.debug("Looking for followers of: %s" % screen_name)
+    logger.debug("Looking for followers of: %s" % user_id)
     for i in range(15):
         cursor = -1
         logger.debug("Getting followers %d/15" % (1+i))
         if len(found_followers) > 0:
             cursor = found_followers[-1].get('next_cursor_str')
         found_followers.append(
-            twitter_api.followers.list(screen_name=screen_name,
-                                                 count=200,
-                                                 cursor=cursor)
-                                                 )
+            twitter_api.followers.list(user_id=user_id,
+                                       count=200,
+                                       cursor=cursor)
+            )
     if destinationFile:
         logger.debug("Writing to file, %s" % destinationFile)
         f = open(destinationFile, 'w')
@@ -46,17 +48,20 @@ def getTopFollowers(foundFollowers=None, user_count=100, source=None):
     logger.debug("Sorting list of %d users" % len(results))
     for result in results:
         for user in result.get('users'):
-            followers.append((user.get('followers_count'), user.get('screen_name')))
+            followers.append((user.get('followers_count'), user.get('str_id')))
     followers = sorted(followers, key=itemgetter(0), reverse=True)
     logger.debug("Returning %d users with most followers." % user_count)
     return followers[:user_count]
 
 
 def followUsers(users):
-    '''Given list of users (screen_name) will be followed.'''
+    '''Given list of users (user_id) will be followed.'''
+    db.init_db()
     for user in users:
         logger.info("Following: %s" % user[1])
-        twitter_api.friendships.create(screen_name=user[1], follow=False)
+        new_user = User()
+        new_user
+        twitter_api.friendships.create(user_id=user[1], follow=False)
 
 
 def main():
