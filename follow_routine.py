553b877f1281b8b53d96fcf64cae7e170540cb47
freko <freko247@gmail.com>
2014-10-14 20:36:53 +0200

    Log exceptions.

--- follow_routine.py
+++ follow_routine.py
@@ -9,31 +9,34 @@ import db
 
 
 def main():
-    follows = 100
-    db.init_db()
-    users = db.session.query(User).all()
-    favorite = None
-    user_list = []
-    logger.info('Starting follow routine.')
-    for user in users:
-        user_list.append(user.user_id)
-        if user.favorited:
-            if user.favorited.date() == datetime.today().date():
-                favorite = user.user_id
-    if favorite:
-        logger.info(
-            "Following todays favorite's (%s) followers" % favorite)
-        favorite_followers = getFollowers(favorite, follows)
-        favorite_followers_ids = [follower.get('id_str') for follower
-                                  in favorite_followers[:follows]
-                                  if follower not in user_list
-                                  and follower == config.TWEET_USER_ID]
-        followUsers(favorite_followers_ids)
-        new_favorite = User()
-        new_favorite.user_id = favorite_followers[0].get('id_str')
-        new_favorite.favorited = datetime.now() + timedelta(days=6)
-    else:
-        logger.info('No favorite for today.')
+    try:
+        follows = 100
+        db.init_db()
+        users = db.session.query(User).all()
+        favorite = None
+        user_list = []
+        logger.info('Starting follow routine.')
+        for user in users:
+            user_list.append(user.user_id)
+            if user.favorited:
+                if user.favorited.date() == datetime.today().date():
+                    favorite = user.user_id
+        if favorite:
+            logger.info(
+                "Following todays favorite's (%s) followers" % favorite)
+            favorite_followers = getFollowers(favorite, follows)
+            favorite_followers_ids = [follower.get('id_str') for follower
+                                      in favorite_followers[:follows]
+                                      if follower not in user_list
+                                      and follower == config.TWEET_USER_ID]
+            followUsers(favorite_followers_ids)
+            new_favorite = User()
+            new_favorite.user_id = favorite_followers[0].get('id_str')
+            new_favorite.favorited = datetime.now() + timedelta(days=6)
+        else:
+            logger.info('No favorite for today.')
+    except Exception, err:
+        logger.error('%s: %s' % (Exception, err))
 
 
 if __name__ == '__main__':
