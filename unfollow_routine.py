8a493d9347140d5a3720b7e27b364707a1662fd2
freko <freko247@gmail.com>
2014-10-14 14:13:16 +0200

    New script unfollows any user that hasn't followed back within 24h.

---
+++ unfollow_routine.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+from models import User
+import db
+from datetime import datetime, timedelta
+from followers import unfollowUsers
+from log import logger
+
+
+def main():
+    logger.info('Running unfollow routine.')
+    db.init_db()
+    q = db.session.query(User).all()
+    unfriend_list = []
+    for user in q:
+        # check if user is unfollowed
+        # chech when user was followed longer than 24h ago
+        # check if user has followed back
+        yesterday = datetime.now()-timedelta(days=1)
+        if not user.unfollowed and \
+                user.followed < yesterday and not \
+                user.followed_back:
+            unfriend_list.append(user.user_id)
+    unfollowUsers(unfriend_list)
+
+
+if __name__ == '__main__':
+    main()
