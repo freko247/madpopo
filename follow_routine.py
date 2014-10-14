f72cf1f68000850893764d61ee61fa61d164962a
freko <freko247@gmail.com>
2014-10-14 15:52:21 +0200

    Only run routine if favorite exists.

--- follow_routine.py
+++ follow_routine.py
@@ -11,20 +11,21 @@ def main():
     users = db.session.query(User).all()
     favorite = None
     user_list = []
+    logger.info('Starting follow routine.')
     for user in users:
         user_list.append(user.user_id)
         if user.favorited.date() == datetime.today().date():
             favorite = user.user_id
             logger.info('Following todays favorites (%s) followers' % favorite)
-
-    favorite_followers = getFollowers(favorite)
-    favorite_followers = [follower for follower
-                          in favorite_followers
-                          if follower not in user_list]
-    followUsers(favorite_followers[:100])
-    new_favorite = User()
-    new_favorite.user_id = favorite_followers[0]
-    new_favorite.favorited = datetime.now() + timedelta(days=6)
-
+            favorite_followers = getFollowers(favorite)
+            favorite_followers = [follower for follower
+                                  in favorite_followers
+                                  if follower not in user_list]
+            followUsers(favorite_followers[:100])
+            new_favorite = User()
+            new_favorite.user_id = favorite_followers[0]
+            new_favorite.favorited = datetime.now() + timedelta(days=6)
+        else:
+            logger.info('No favorite for today.')
 if __name__ == '__main__':
     main()
