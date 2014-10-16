29acbb10b909b09184cce78949fb29ec1362c3b5
freko <freko247@gmail.com>
2014-10-16 19:51:40 +0200

    Get friend list from database.

--- friend_updater.py
+++ friend_updater.py
@@ -1,11 +1,16 @@
 # -*- coding:utf-8 -*-
+import db
 import friends
 from log import logger
+from models import User
 
 
 def main():
     logger.info('Starting friend updater.')
-    friends_list = friends.getFriends()
+    db.init_db()
+    friends_list = []
+    for friend in db.session.query(User).all():
+        friends_list.append(friend.user_id)
     friends.update_friends(friends_list)
 
 
