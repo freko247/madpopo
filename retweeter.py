6e47a8ddb0ea713b6e4dd2089343ad65a748dec5
freko <freko247@gmail.com>
2014-10-17 19:14:23 +0200

    Retweet 0-3 tweets with 0-5 miutes interval, 0-15 minutes after script execution.

--- retweeter.py
+++ retweeter.py
@@ -2,7 +2,7 @@
 import datetime
 from operator import itemgetter
 from random import randrange
-import time
+from time import sleep
 
 import analysis
 import config
@@ -12,9 +12,17 @@ from models import ReTweet
 from tweets import getTweets, reTweet
 
 
+INITIAL_SLEEP_MAX = 15
+RETWEET_COUNT_MAX = 3
+TWEET_SLEEP_MAX = 5
+
+
 def main():
     try:
-        logger.info('Starting retweet script')
+        first_sleep = randrange(INITIAL_SLEEP_MAX)
+        logger.info('Starting retweet script, but first sleeping %d minutes'
+                    % first_sleep)
+        sleep(60*sleep_time)
         # Get tweets
         tweets = getTweets(config.TWEET_LIKES)
         # Analyze tweets
@@ -29,18 +37,27 @@ def main():
                                  )
                                 )
         analyzed = sorted(analyzed, key=itemgetter(0), reverse=True)
-        # Retweet the most positive tweet
-        tweet = analyzed[0]
-        reTweet(tweet[1])
-        new_retweet = ReTweet()
-        new_retweet.tweet_id = tweet[1]
-        new_retweet.sentiment = tweet[0]
-        new_retweet.retweet_date = datetime.datetime.now()
-        db.session.add(new_retweet)
-        db.session.commit()
-        logger.info(
-            'Retweeting tweet_id: %s, with sentiment %s ' % (
-                analyzed[0][1], analyzed[0][0]))
+        # Retweet the most positive tweets
+        retweet_count = randrange(RETWEET_COUNT_MAX)
+        logger.info('Retweeting %d tweets' % retweet_count)
+        for i in range(retweet_count):
+            tweet = analyzed[i]
+            reTweet(tweet[1])
+            new_retweet = ReTweet()
+            new_retweet.tweet_id = tweet[1]
+            new_retweet.sentiment = tweet[0]
+            new_retweet.retweet_date = datetime.datetime.now()
+            db.session.add(new_retweet)
+            db.session.commit()
+            sleep_time = randrange(TWEET_SLEEP_MAX)
+            logger.info(
+                'Retweeting %d/%d tweet_id: %s, with sentiment %s and sleeping'
+                ' for %d minutes' % (i+1,
+                                     retweet_count,
+                                     analyzed[0][1],
+                                     analyzed[0][0],
+                                     sleep_time))
+            sleep(60*sleep_time)
     except Exception, err:
         logger.error('%s: %s' % (Exception, err))
 
