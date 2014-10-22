d3affb5614d4a653f640849e7fa6f39b2aa749e8
freko <freko247@gmail.com>
2014-10-22 10:02:46 +0200

    Acutally use new favorite as favorite.

--- follow_routine.py
+++ follow_routine.py
@@ -30,17 +30,18 @@ def main():
             new_favorite.favorited = datetime.now()
             db.session.merge(new_favorite)
             db.session.commit()
-            logger.debug(
-                "Following todays favorite's (%s) followers" % favorite)
-            favorite_followers = getFollowers(favorite, follows)
-            favorite_followers_ids = [follower.get('id_str') for follower
-                                      in favorite_followers[:follows]
-                                      if (follower not in user_list
-                                      and follower != config.TWEET_USER_ID)]
-            followUsers(favorite_followers_ids)
-            new_favorite = User()
-            new_favorite.user_id = favorite_followers[0].get('id_str')
-            new_favorite.favorited = datetime.now() + timedelta(days=6)
+            favorite = new_favorite.user_id
+        logger.debug(
+            "Following todays favorite's (%s) followers" % favorite)
+        favorite_followers = getFollowers(favorite, follows)
+        favorite_followers_ids = [follower.get('id_str') for follower
+                                  in favorite_followers[:follows]
+                                  if (follower not in user_list
+                                  and follower != config.TWEET_USER_ID)]
+        followUsers(favorite_followers_ids)
+        new_favorite = User()
+        new_favorite.user_id = favorite_followers[0].get('id_str')
+        new_favorite.favorited = datetime.now() + timedelta(days=6)
     except Exception, err:
         logger.error('%s: %s' % (Exception, err))
 
