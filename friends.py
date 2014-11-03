9dfdd8cc6efb075ef52d594566e4e57f45302bdb
freko <freko247@gmail.com>
2014-11-03 23:03:51 +0100

    Store information about users, but not text that can't be decoded with utf-8.

--- friends.py
+++ friends.py
@@ -45,14 +45,59 @@ def update_users(users, user_followers=False):
             updated_user.followed_back = datetime.now()
         join_date = parser.parse(user['created_at']).replace(tzinfo=None)
         updated_user.join_date = join_date
-        updated_user.statuses_count = user['statuses_count']
-        updated_user.friends_count = user['friends_count']
-        updated_user.followers_count = user['followers_count']
-        updated_user.language = get_language(user['lang'])
-        # TODO add following values...
-        # activity = Column(Float())          # tweet_frequency
-        # followed = Column(DateTime)         # Date when user was followed
-        # followed_back = Column(DateTime)    # Date when user followed back
+        updated_user.contributors_enabled = user.get('contributors_enabled')
+        updated_user.default_profile = user.get('default_profile')
+        updated_user.default_profile_image = user.get('default_profile_image')
+        updated_user.favourites_count = user.get('favourites_count')
+        updated_user.follow_request_sent = user.get('follow_request_sent')
+        updated_user.followers_count = user.get('followers_count')
+        updated_user.following = user.get('following')
+        updated_user.friends_count = user.get('friends_count')
+        updated_user.geo_enabled = user.get('geo_enabled')
+        updated_user.is_translator = user.get('is_translator')
+        updated_user.language = get_language(user.get('lang'))
+        updated_user.listed_count = user.get('listed_count')
+        updated_user.profile_background_color = user.get(
+            'profile_background_color')
+        updated_user.profile_background_image_url = user.get(
+            'profile_background_image_url')
+        updated_user.profile_background_image_url_https = user.get(
+            'profile_background_image_url_https')
+        updated_user.profile_background_tile = user.get(
+            'profile_background_tile')
+        updated_user.profile_banner_url = user.get('profile_banner_url')
+        updated_user.profile_image_url = user.get('profile_image_url')
+        updated_user.profile_image_url_https = user.get(
+            'profile_image_url_https')
+        updated_user.profile_link_color = user.get('profile_link_color')
+        updated_user.profile_sidebar_border_color = user.get(
+            'profile_sidebar_border_color')
+        updated_user.profile_sidebar_fill_color = user.get(
+            'profile_sidebar_fill_color')
+        updated_user.profile_text_color = user.get('profile_text_color')
+        updated_user.profile_use_background_image = user.get(
+            'profile_use_background_image')
+        updated_user.protected = user.get('protected')
+        updated_user.screen_name = user.get('screen_name')
+        updated_user.show_all_inline_media = user.get('show_all_inline_media')
+        updated_user.statuses_count = user.get('statuses_count')
+        updated_user.time_zone = user.get('time_zone')
+        updated_user.url = user.get('url')
+        updated_user.utc_offset = user.get('utc_offset')
+        updated_user.verified = user.get('verified')
+        updated_user.withheld_in_countries = user.get('withheld_in_countries')
+        updated_user.withheld_scope = user.get('withheld_scope')
+
+        for attribute in ['description', 'name', 'location']:
+            try:
+                data = user.get(attribute)
+                data.decode('utf-8')
+                updated_user.__setattr__(attribute, data)
+            except:
+                # logger.debug('Encoding error for %s for user: %s' %
+                #              (attribute, updated_user.screen_name))
+                pass
+
         db.session.merge(updated_user)
         db.session.commit()
     logger.info('Updating %d users' % len(users))
