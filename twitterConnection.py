aa2df313479b325e868d865cc92eff0d9e49970f
freko <freko247@gmail.com>
2014-11-13 21:59:21 +0100

    Easily create connections with different api applications with new function get_api_connection

--- twitterConnection.py
+++ twitterConnection.py
@@ -9,6 +9,19 @@ twitter_api = twitter.Twitter(auth=auth)
 
 
 class TwitterStream():
+
     """Class for creating differrent stream connections"""
+
     def __init__(self, domain, auth=auth, ):
         self.stream = twitter.TwitterStream(domain=domain, auth=auth)
+
+
+def get_api_connection(app):
+    credentials = config.OAUTH[app]
+    auth = twitter.oauth.OAuth(
+        credentials.get('OAUTH_TOKEN'),
+        credentials.get('OAUTH_TOKEN_SECRET'),
+        credentials.get('CONSUMER_KEY'),
+        credentials.get('CONSUMER_SECRET')
+    )
+    return twitter.Twitter(auth=auth)
