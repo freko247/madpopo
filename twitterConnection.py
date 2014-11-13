211a934b98ab57ab0531f242b6dcc1141c113ef1
freko <freko247@gmail.com>
2014-11-13 22:47:39 +0100

    Create new twitterstream with chosen credentials.

--- twitterConnection.py
+++ twitterConnection.py
@@ -2,6 +2,7 @@
 import twitter
 import config
 
+
 auth = twitter.oauth.OAuth(config.OAUTH_TOKEN, config.OAUTH_TOKEN_SECRET,
                            config.CONSUMER_KEY, config.CONSUMER_SECRET)
 
@@ -12,8 +13,18 @@ class TwitterStream():
 
     """Class for creating differrent stream connections"""
 
-    def __init__(self, domain, auth=auth, ):
-        self.stream = twitter.TwitterStream(domain=domain, auth=auth)
+    def __init__(self, domain, app=None):
+        stream_auth = None
+        if app:
+            credentials = config.OAUTH[app]
+            stream_auth = twitter.oauth.OAuth(
+                credentials.get('OAUTH_TOKEN'),
+                credentials.get('OAUTH_TOKEN_SECRET'),
+                credentials.get('CONSUMER_KEY'),
+                credentials.get('CONSUMER_SECRET')
+            )
+        self.stream = twitter.TwitterStream(
+            domain=domain, auth=stream_auth or auth)
 
 
 def get_api_connection(app):
