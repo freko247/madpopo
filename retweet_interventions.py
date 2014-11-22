f5cb98f1459984230aed689863034730f019a259
freko <freko247@gmail.com>
2014-11-22 07:27:01 +0100

    Added todays interventions

--- retweet_interventions.py
+++ retweet_interventions.py
@@ -56,6 +56,7 @@ TAGS = [(datetime.date(2014, 11, 13), '#madpopo79'),
         (datetime.date(2014, 11, 14), '#getyourflushot'),
         (datetime.date(2014, 11, 17), '#highfiveastranger'),
         (datetime.date(2014, 11, 19), '#somethinggood, #unicef'),
+        (datetime.date(2014, 11, 21), '#HowManyPushups'),
         ]
 
 
